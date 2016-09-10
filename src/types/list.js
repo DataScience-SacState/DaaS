4448dd87b10bf08a5cf632e3e1605e4a40af8fd4
MatthewMerrill <MattMerr47@gmail.com>
2016-09-10 00:09:29 -0700

    Refactor typeMap -> abstract typeConfig

--- src/types/list.js
+++ src/types/list.js
@@ -1,43 +1,40 @@
+const typeConfig = require('../typeConfig');
+
 /*
 list?format={
-	"type": "list",
-	"count": 10,
-	"format": {
-		"bool": { "type": "bool" }
-	}
+    "type": "list",
+    "count": 10,
+    "format": {
+        "bool": { "type": "bool" }
+    }
 }
 */
 
 var data = {
-	date: {
-		type: "date",
-		min: "2013-09-23",
-		max: "2015-08-12"
-	}
+    date: {
+        type: "date",
+        min: "2013-09-23",
+    	max: "2015-08-12"
+    }
 }
 
 function list(params) { 
-	var params = JSON.parse(params.format);	
-	var list = [];
-	for(var i = 0; i < params.count; i++) {
-		list.push(createFormatObj(params.format))
-	}
-	return list;
-
+    var params = JSON.parse(params.format);
+    var list = [];
+    for(var i = 0; i < params.count; i++) {
+        list.push(createFormatObj(params.format))
+    }
+    return list;
 }
 
 function createFormatObj(format) {
-	console.log(format)
-	const typeFuncs = require('../typeMap');
-	console.log(typeFuncs)
-	var obj = {};
-	for (var key in format) {
-		var settings = format[key];
-		var settingsFunc = typeFuncs[settings.type]
-		console.log(settingsFunc)
-		if (settingsFunc) obj[key] = settingsFunc(settings);
-	}
-	return obj;
+    var obj = {};
+    for (var key in format) {
+        var settings = format[key];
+        var settingsFunc = typeConfig.getFunc(settings.type);
+        if (settingsFunc) obj[key] = settingsFunc(settings);
+    }
+    return obj;
 }
 
 module.exports = list;
